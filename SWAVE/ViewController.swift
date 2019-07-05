897969cad2634f375ac8de4638a0a01f0a3cff18
San Nguyen <mr.sn5@icloud.com>
2019-07-05 15:40:14 +0300

    Initial Commit

---
+++ SWAVE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SWAVE
+//
+//  Created by San Byn Nguyen on 7/5/19.
+//  Copyright © 2019 San Byn Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
